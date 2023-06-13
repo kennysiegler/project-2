603b1300ec5015eaa9085530ad3ea1ac1fe21a43
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 <kennysiegler@Kennys-MacBook-Pro.local>
2023-06-12 20:35:24 -0500

    Updated SQL File

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,46 +1,60 @@
+-- Categories
+CREATE TABLE IF NOT EXISTS public.categories
+(
+    category_id character varying(60)  NOT NULL,
+    category character varying(60)  NOT NULL,
+    CONSTRAINT categories_pkey PRIMARY KEY (category_id)
+);
+
+
+-- SUBCATEGORIES
+CREATE TABLE IF NOT EXISTS public.subcategories
+(
+    subcategory_id character varying(60)  NOT NULL,
+    subcategory character varying(60)  NOT NULL,
+    CONSTRAINT subcategories_pkey PRIMARY KEY (subcategory_id)
+);
+
+-- CONTACT
+CREATE TABLE IF NOT EXISTS public.contacts
+(
+    contact_id integer NOT NULL,
+    first_name character varying(30)  NOT NULL,
+    last_name character varying(30)  NOT NULL,
+    email character varying(60) NOT NULL,
+    CONSTRAINT contacts_pkey PRIMARY KEY (contact_id)
+);
 -- CAMPAIGNS
-CREATE TABLE campaigns (
+CREATE TABLE IF NOT EXISTS public.campaigns
+(
     cf_id integer NOT NULL,
     contact_id integer NOT NULL,
-    company_name character varying[] COLLATE pg_catalog."default" NOT NULL,
-    description character varying[] COLLATE pg_catalog."default" NOT NULL,
-    goal integer NOT NULL,
-    pledged integer NOT NULL,
-    outcome character varying[] COLLATE pg_catalog."default" NOT NULL,
-    backers_count integer NOT NULL,
-    country character varying[] COLLATE pg_catalog."default" NOT NULL,
-    currency character varying[] COLLATE pg_catalog."default" NOT NULL,
-    launch_date date[] NOT NULL,
-    end_date date[] NOT NULL,
-    category_id character varying[] COLLATE pg_catalog."default" NOT NULL,
-    subcategory_id character varying[] COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT campaign_pkey PRIMARY KEY (cf_id)
-)
-;
-
---CATEGORIES
-CREATE TABLE categories (
-	category_id varchar(60) NOt NULL,
-	category varchar(60) NOT NULL,
-	PRIMARY KEY (category_id)
+    company_name character varying(60) NOT NULL,
+    description character varying(60) NOT NULL,
+    goal double precision,
+    pledged double precision,
+    outcome character varying(60) NOT NULL,
+    backers_count integer,
+    country character varying(60) NOT NULL,
+    currency character varying(60) NOT NULL,
+    launch_date date NOT NULL,
+    end_date date NOT NULL,
+    category_id character varying(60) NOT NULL,
+    subcategory_id character varying(60) NOT NULL,
+    CONSTRAINT campaigns_pkey PRIMARY KEY (cf_id),
+    CONSTRAINT campaigns_category_id_fkey FOREIGN KEY (category_id)
+        REFERENCES public.categories (category_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT campaigns_contact_id_fkey FOREIGN KEY (contact_id)
+        REFERENCES public.contacts (contact_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT campaigns_subcategory_id_fkey FOREIGN KEY (subcategory_id)
+        REFERENCES public.subcategories (subcategory_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
---SUBCATEGORIES
-CREATE TABLE subcategories (
-	subcategory_id varchar(60) NOt NULL,
-	subcategory varchar(60) NOT NULL,
-	PRIMARY KEY (subcategory_id)
-);
-
---CONTACT
-CREATE TABLE contacts (
-	contact_id integer NOT NULL,
-	first_name varchar(30) NOT NULL,
-	last_name varchar(30) NOT NULL,
-	email varchar(60) NOT NULL,
-	PRIMARY KEY (contact_id)
-);
-
-
 
 
 
